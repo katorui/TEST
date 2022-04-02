82bc76f931848097588debfea2bd53dbafc763bb
加藤瑠依 <rui14jpn@gmail.com>
2022-04-02 18:38:43 +0900

    変更

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+try {
+    $dbh = new PDO("mysql:host=localhost;dbname=raspai_test;charset=utf8mb4","root","root");
+    $stmt = $dbh->prepare("SELECT * FROM item");
+    $stmt->execute();
+    $all = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    return "エラー：" . htmlspecialchars($e->getMessage(),
+    ENT_QUOTES, 'UTF-8') . "<br>";
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,11 +18,15 @@
   <title>Document</title>
 </head>
 <body>
-    <h1>トップページです</h1>
-    <h4>takeブランチです2</h4>
-    <h4>takeブランチです3</h4>
-    <div>
-      <a href="other.php">otherページへ</a>
-    </div>
+<h1>アイテム追加</h1>
+<form action = "other.php" method="POST">
+    <input type="text" name ="item_name">
+    <input type="submit" value="送信">
+</form>
+<?php foreach ($all as $item) :?>
+    <ul>
+      <li><?php echo $item["item_name"]; ?></li>
+    </ul>
+<?php endforeach; ?>
 </body>
 </html>
